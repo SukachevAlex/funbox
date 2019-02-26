0456e3d49139919441aea2fed8f0ace2742830da
SukachevAlex <sukachevalexandr@gmail.com>
2019-02-26 17:26:47 +0300

    Add exo font to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { cn } from '@bem-react/classname';
 import React, { Component } from 'react';
 import './App.css';
+
+const cnPage = cn('Page');
 
 class App extends Component {
   render() {
     return (
-      <div></div>
+      <div className={cnPage()}></div>
     );
   }
 }
