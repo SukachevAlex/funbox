12f35a5826d67f20219b7d95268c860b78093758
SukachevAlex <sukachevalexandr@gmail.com>
2019-02-27 22:30:57 +0300

    Add project to gh-pages

---
+++ build/precache-manifest.a307fcceb79a7e339174942e08d5d2c8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f770d64d2736e8a4defb37d17a566579",
+    "url": "./static/media/exo-2-v5-cyrillic-100.f770d64d.woff"
+  },
+  {
+    "revision": "db29d761ad391f4ad43c3b37df99b70e",
+    "url": "./static/media/exo-2-v5-cyrillic-100.db29d761.svg"
+  },
+  {
+    "revision": "557a88374d1e8c958919a03d66a71379",
+    "url": "./static/media/exo-2-v5-cyrillic-100.557a8837.ttf"
+  },
+  {
+    "revision": "4e556de1a07c9551032dc9fddd7eb54a",
+    "url": "./static/media/exo-2-v5-cyrillic-100.4e556de1.eot"
+  },
+  {
+    "revision": "09554dbf9f05318184de09f8d838e36b",
+    "url": "./static/media/exo-2-v5-cyrillic-100.09554dbf.woff2"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "aa17257675670b4fa9bf",
+    "url": "./static/js/main.aa172576.chunk.js"
+  },
+  {
+    "revision": "be17a27d3e54bf8d02b3",
+    "url": "./static/js/2.be17a27d.chunk.js"
+  },
+  {
+    "revision": "aa17257675670b4fa9bf",
+    "url": "./static/css/main.2597f91b.chunk.css"
+  },
+  {
+    "revision": "71033981b31416fb6bb02e8b8c8970dc",
+    "url": "./index.html"
+  }
+];
